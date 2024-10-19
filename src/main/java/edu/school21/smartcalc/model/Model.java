d2b939cf5af8f666338d90303a5f6e0cefac8eb3
Максим Кудиенко <93645634+fatherofsevenchildren@users.noreply.github.com>
2024-10-20 01:28:24 +0500

    Update Model.java

--- src/main/java/edu/school21/smartcalc/model/Model.java
+++ src/main/java/edu/school21/smartcalc/model/Model.java
@@ -2,14 +2,9 @@ package edu.school21.smartcalc.model;
 
 public class Model {
 
-    /*
-        Здесь временно нужно вписать абсолютный путь до библиотеки(
-     */
-
     static {
-        System.load("/Users/leonidak/progects/APJ2_SmartCalc_v3.0_Desktop_Java-1/src/SmartCalc/src/main/resources/cpplib/libnative.dylib");
+        System.load("/cpplib/libnative.dylib");
     }
-
 
     public native String performCalculation(String expression, String xExpression);
 }
